099196165a5f0522be735d00737e3be877889482
sirharwell <36307972+sirharwell@users.noreply.github.com>
2025-08-18 20:08:06 -0700

    might be working

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./LoginPage";
 import Scorecard from "./Scorecard";
-import React from "react"; 
+import React from "react";
 
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/scorecard" element={<Scorecard />} />
-      </Routes>
-    </BrowserRouter>
+    <div>
+      <BrowserRouter basename="/panda-royale">
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/scorecard" element={<Scorecard />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
+
 
 export default App;
