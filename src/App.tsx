bedf1f774516ccacb20917698e27a548e8d48b34
sirharwell <36307972+sirharwell@users.noreply.github.com>
2025-08-20 00:19:13 -0700

    Styling

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,15 @@ import { HashRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./LoginPage";
 import Scorecard from "./Scorecard";
 
-console.log("App loaded ✅");
 function App() {
   return (
     <HashRouter>
+    <div className="min-h-screen bg-cover bg-center" style={{ backgroundImage: "url('/bamboo.png')" }}>
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/scorecard" element={<Scorecard />} />
       </Routes>
+      </div>
     </HashRouter>
   );
 }
